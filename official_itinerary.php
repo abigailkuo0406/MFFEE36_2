4c36076cfbe04ead78b9f9569e455a12b7d1d934
tsao <yo860107@gmail.com>
2023-05-15 15:35:19 +0800

    tsa0_lost.php

--- official_itinerary.php
+++ official_itinerary.php
@@ -2,15 +2,17 @@
 <title>Test for Layout</title><!-- 網頁標題可自行修改-->
 <style>
     /* CSS可以自行修改 */
+
 </style>
 </head>
 
 <body>
 
+
     <?php include './parts/navbar.php' ?>
     <div class="main_screen d-flex justify-content-between">
         <button id="OffcanvasNav" class="btn" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasScrolling" aria-controls="offcanvasScrolling"><i class="fa-solid fa-caret-right"></i></button>
-
+           
         <?php include './parts/tsao_parts/tsao_list.php' ?> <!-- 路徑可自行修改-->
     </div>
 
