82bc482d5b7d6a71a9e98d94873ae273a9db7102
John <japanwork@gmail.com>
2023-05-16 17:10:01 +0800

    NEW version

--- parts/john_parts/back/part/navbar.php
+++ parts/john_parts/back/part/navbar.php
@@ -28,10 +28,10 @@
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link <?= $pageName == 'list' ? 'active' : ' ' ?> " href="list.php">列表</a>
+                        <a class="nav-link <?= $pageName == 'list' ? 'active' : ' ' ?> " href="account.php">列表</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link <?= $pageName == 'add' ? 'active' : ' ' ?>" href="add.php">新增</a>
+                        <a class="nav-link <?= $pageName == 'add' ? 'active' : ' ' ?>" href="account_add.php">新增</a>
                     </li>
                 </ul>
             </div>
