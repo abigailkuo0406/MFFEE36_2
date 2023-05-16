2b9be3de0b21b1230dd3fb521c27e7a83cc6a178
abigailkuo <abigailkuo0406@gmail.com>
2023-05-16 21:59:25 +0800

    刪除

--- parts/john_parts/back/index_.php
+++
@@ -1,6 +0,0 @@
-<?php include './parts/html-head.php'; ?>
-<?php include './parts/navbar.php'; ?>
-<div class="container">123456</div>
-<?php include './parts/scripts.php'; ?>
-<script></script>
-<?php include './parts/html-foot.php'; ?>
--- parts/john_parts/back/part/scripts.php
+++
@@ -1,1 +0,0 @@
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
