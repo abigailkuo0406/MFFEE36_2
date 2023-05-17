f44ca4b6278680c8137feab0e2f9c588e8449ac5
Abigail <christinekuo905@gmail.com>
2023-05-17 20:57:34 +0800

    搜尋欄功能完成

--- parts/kuo_parts/restaurant_list.php
+++ parts/kuo_parts/restaurant_list.php
@@ -2,57 +2,95 @@
 // 連線資料庫
 require './parts/kuo_parts/restaurant_connect-db.php';
 
-// 每頁要顯示的資料數量
-$perPage = 5;
-
-// 使用者當前查看的頁面是第幾頁
-$page = isset($_GET['page']) ? intval($_GET['page']) : 1;
-
-#限制網址列輸入零或負數頁碼要跳回第一頁
-if ($page < 1) {
-    header('Location:?page=1');
-    exit;
-}
-
-#計算總筆數
-$t_sql = "SELECT COUNT(1) FROM restaurant_list";
-$totalRows = $pdo->query($t_sql)->fetch(PDO::FETCH_NUM)[0];
-
-// 計算總頁數
-$totalPage = ceil($totalRows / $perPage);
-
-$rows = [];
-
-
-// 如果資料庫有資料再做資料撈取跟顯示
-if ($totalRows) {
-
-    $sql = sprintf("SELECT * FROM restaurant_list LIMIT %s,%s", ($page - 1) * $perPage, $perPage);
-    #依照在第幾頁，撈取對應資料，例如第一頁顯示1-10筆資料，第二頁顯示第11-20筆資料
-
-    $rows = $pdo->query($sql)->fetchAll();
-
-    // 如果當前頁碼大於總頁數
-    if ($page > $totalPage) {
-        header("Location:?page=$totalPage");
-    }
-}
-
 // 取地區資料
 $sql_area = "SELECT * FROM area_list WHERE 1";
 $areaArray = $pdo->query($sql_area)->fetchAll();
-
-// 搜尋後顯示資料
-$search = isset($_GET['search']) ? $_GET['search'] : null;
-if ($search) {
-    $search_sql = sprintf("SELECT COUNT(1) FROM restaurant_list WHERE rest_area='%s'", $search);
-    $total_search_row = $pdo->query($search_sql)->fetch(PDO::FETCH_NUM)[0];
-}
-
 ?>
 
-<!-- 頁面呈現 -->
-<div class="container">
+<div class="container mt-5">
+    <form class="input-group mb-3" method="get">
+        <div class="mb-3">
+            <label for="search" class="form-label" class="form-control">以縣市搜尋</label>
+
+            <select name="search" id="search" class="form-select" aria-label="Default select example" data-required="2">
+                <option selected>--請選擇--</option>
+                <?php foreach ($areaArray as $i) : ?>
+                    <option value="<?= isset($i['area_name']) ? $i['area_name'] : null ?>"><?= $i['area_name'] ?></option>
+                <?php endforeach ?>
+
+            </select>
+            <input type="submit" value="搜尋">
+        </div>
+    </form>
+
+
+    <?php
+    // 每頁要顯示的資料數量
+    $perPage = 5;
+
+    // 使用者當前查看的頁面是第幾頁,強制轉int為了不讓人拿字串亂試網址
+    $page = isset($_GET['page']) ? intval($_GET['page']) : 1;
+
+    // 搜尋後顯示資料
+    $search = isset($_GET['search']) ? $_GET['search'] : null;
+    // echo $search;
+    // exit;
+    if ($search) {
+        $search_sql = sprintf("SELECT COUNT(1) FROM restaurant_list WHERE rest_area='%s'", $search);
+        $total_search_row = $pdo->query($search_sql)->fetch(PDO::FETCH_NUM)[0];
+        // echo $total_search_row;
+    }
+
+
+    if ($total_search_row) {
+        // 計算總頁數
+        $totalPage = ceil($total_search_row / $perPage);
+        $rows = [];
+        $sql = sprintf("SELECT * FROM restaurant_list WHERE rest_area='%s' LIMIT %s,%s", $search, ($page - 1) * $perPage, $perPage);
+        #依照在第幾頁，撈取對應資料，例如第一頁顯示1-10筆資料，第二頁顯示第11-20筆資料
+
+        $rows = $pdo->query($sql)->fetchAll();
+
+
+        // 如果當前頁碼大於總頁數
+        if ($page > $totalPage) {
+            header("Location:?page=$totalPage");
+        }
+    } else {
+        #計算總筆數
+        $t_sql = "SELECT COUNT(1) FROM restaurant_list";
+        $totalRows = $pdo->query($t_sql)->fetch(PDO::FETCH_NUM)[0];
+
+        // 計算總頁數
+        $totalPage = ceil($totalRows / $perPage);
+
+        $rows = [];
+
+        // 如果資料庫有資料再做資料撈取跟顯示
+        if ($totalRows) {
+
+            $sql = sprintf("SELECT * FROM restaurant_list LIMIT %s,%s", ($page - 1) * $perPage, $perPage);
+            #依照在第幾頁，撈取對應資料，例如第一頁顯示1-10筆資料，第二頁顯示第11-20筆資料
+
+            $rows = $pdo->query($sql)->fetchAll();
+
+            // 如果當前頁碼大於總頁數
+            if ($page > $totalPage) {
+                header("Location:?page=$totalPage");
+            }
+        }
+    }
+    #限制網址列輸入零或負數頁碼要跳回第一頁
+    if ($page < 1) {
+        header('Location:?page=1');
+        exit;
+    }
+
+
+    ?>
+
+    <!-- 頁面呈現 -->
+
 
 
     <!-- <div class="input-group me-auto my-5">
@@ -64,10 +102,7 @@ if ($search) {
         </span>
     </div> -->
 
-    <form class="input-group mb-3" method="GET">
-        <input name="search" type="text" class="form-control" placeholder="輸入關鍵字" value="<?= isset($_GET['search']) ? $_GET['search'] : null ?>" aria-label="Recipient's username" aria-describedby="button-addon2">
-        <button class="btn btn-outline-secondary" type="submit" id="button-addon2"><i class="fa-solid fa-magnifying-glass"></i></button>
-    </form>
+
 
 
     <div class="row">
