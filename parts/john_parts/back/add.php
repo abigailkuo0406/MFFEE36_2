41437f66fa47df0b948f077d63f0dffbc0796552
John <japanwork@gmail.com>
2023-05-18 13:24:40 +0800

    new

--- parts/john_parts/back/add.php
+++ parts/john_parts/back/add.php
@@ -20,103 +20,177 @@ require './parts/john_parts/back/part/connect-db.php';
     <div class="row">
         <div class="col-6">
             <div class="card">
-
                 <div class="card-body">
                     <h5 class="card-title">新增資料</h5>
                     <form name="form1" onsubmit="checkForm(event)">
                         <div class="mb-3">
-                            <label for="email" class="form-label">Email</label>
+                            <label for="email" class="form-label">帳號</label>
                             <input type="email" class="form-control" id="email" name="email" autocomplete="on">
                             <div class="form-text"></div>
                         </div>
                         <div class="mb-3">
-                            <label for="password" class="form-label">password</label>
+                            <label for="password" class="form-label">密碼</label>
                             <input type="text" class="form-control" id="password" name="password">
                             <div class="form-text"></div>
                         </div>
                         <div class="mb-3">
-                            <label for="images" class="form-label">images</label>
+                            <label for="images" class="form-label">大頭貼</label>
                             <input type="number" class="form-control" id="images" name="images">
                             <div class="form-text"></div>
                         </div>
                         <div class="mb-3">
-                            <label for="member_name" class="form-label">Name</label>
+                            <label for="member_name" class="form-label">名字</label>
                             <input type="text" class="form-control" id="member_name" name="member_name" value="<?= isset($_POST['member_name']) ? htmlentities($_POST['member_name']) : '' ?>">
                             <div class="form-text"></div>
                         </div>
                         <div class="mb-3">
-                            <label for="member_birth" class="form-label">birthday</label>
+                            <label for="member_birth" class="form-label">生日</label>
                             <input type="date" class="form-control" id="member_birth" name="member_birth">
                             <div class="form-text"></div>
                         </div>
                         <div class="mb-3">
-                            <label for="id_number" class="form-label">id_number</label>
+                            <label for="id_number" class="form-label">身分證字號</label>
                             <input type="text" class="form-control" id="id_number" name="id_number">
                             <div class="form-text"></div>
                         </div>
                         <div class="mb-3">
-                            <label for="gender" class="form-label">gender</label>
-                            <input type="text" class="form-control" id="gender" name="gender">
-                            <div class="form-text"></div>
+                            <label for="gender" class="form-label">性別</label>
+                            <div class="form-check">
+                                <input class="form-check-input" type="radio" name="gender" id="gender" value="男" checked>
+                                <label class="form-check-label" for="flexRadioDefault1">
+                                    男
+                                </label>
+                            </div>
+                            <div class="form-check">
+                                <input class="form-check-input" type="radio" name="gender" id="gender" value="女">
+                                <label class="form-check-label" for="flexRadioDefault2">
+                                    女
+                                </label>
+                            </div>
                         </div>
                         <div class="mb-3">
-                            <label for="location" class="form-label">location</label>
+                            <label for="location" class="form-label">居住地</label>
                             <input type="text" class="form-control" id="location" name="location">
                             <div class="form-text"></div>
                         </div>
                         <div class="mb-3">
-                            <label for="height" class="form-label">height</label>
+                            <label for="height" class="form-label">身高</label>
                             <input type="text" class="form-control" id="height" name="height">
                             <div class="form-text"></div>
                         </div>
                         <div class="mb-3">
-                            <label for="weight" class="form-label">weight</label>
+                            <label for="weight" class="form-label">體重</label>
                             <input type="text" class="form-control" id="weight" name="weight">
                             <div class="form-text"></div>
                         </div>
                         <div class="mb-3">
-                            <label for="zodiac" class="form-label">zodiac</label>
-                            <input type="text" class="form-control" id="zodiac" name="zodiac">
-                            <div class="form-text"></div>
+                            <label for="zodiac" class="form-label">星座</label>
+                            <select class="form-select" multiple aria-label="multiple select example" name="zodiac">
+                                <option value="牡羊座" selected>牡羊座(3/21~4/19)</option>
+                                <option value="金牛座">金牛座(4/20~5/20)</option>
+                                <option value="雙子座">雙子座(5/21~6/21)</option>
+                                <option value="巨蟹座">巨蟹座(6/22~7/22)</option>
+                                <option value="獅子座">獅子座(7/23~8/22)</option>
+                                <option value="處女座">處女座(8/23~9/23)</option>
+                                <option value="天秤座">天秤座(9/24~10/23)</option>
+                                <option value="天蠍座">天蠍座(10/24~11/22)</option>
+                                <option value="射手座">射手座(11/23~12/21)</option>
+                                <option value="魔羯座">魔羯座(12/22~1/20)</option>
+                                <option value="水瓶座">水瓶座 (1/21~2/18)</option>
+                                <option value="雙魚座">雙魚座(2/18~3/20)</option>
+                            </select>
                         </div>
                         <div class="mb-3">
-                            <label for="bloodtype" class="form-label">bloodtype</label>
-                            <input type="text" class="form-control" id="bloodtype" name="bloodtype">
-                            <div class="form-text"></div>
+                            <label for="bloodtype" class="form-label">血型</label>
+                            <div class="form-check">
+                                <input class="form-check-input" type="radio" name="bloodtype" id="bloodtype" value="A型" checked>
+                                <label class="form-check-label" for="flexRadioDefault1">
+                                    A型
+                                </label>
+                            </div>
+                            <div class="form-check">
+                                <input class="form-check-input" type="radio" name="bloodtype" id="bloodtype" value="B型">
+                                <label class="form-check-label" for="flexRadioDefault2">
+                                    B型
+                                </label>
+                            </div>
+                            <div class="form-check">
+                                <input class="form-check-input" type="radio" name="bloodtype" id="bloodtype" value="AB型">
+                                <label class="form-check-label" for="flexRadioDefault2">
+                                    AB型
+                                </label>
+                            </div>
+                            <div class="form-check">
+                                <input class="form-check-input" type="radio" name="bloodtype" id="bloodtype" value="O型">
+                                <label class="form-check-label" for="flexRadioDefault2">
+                                    O型
+                                </label>
+                            </div>
                         </div>
                         <div class="mb-3">
-                            <label for="smoke" class="form-label">smoke</label>
-                            <input type="text" class="form-control" id="smoke" name="smoke">
-                            <div class="form-text"></div>
+                            <label for="smoke" class="form-label">抽菸</label>
+                            <div class="form-check">
+                                <input class="form-check-input" type="radio" name="smoke" id="smoke" value="有" checked>
+                                <label class="form-check-label" for="flexRadioDefault1">
+                                    有
+                                </label>
+                            </div>
+                            <div class="form-check">
+                                <input class="form-check-input" type="radio" name="smoke" id="smoke" value="沒有">
+                                <label class="form-check-label" for="flexRadioDefault2">
+                                    沒有
+                                </label>
+                            </div>
                         </div>
                         <div class="mb-3">
-                            <label for="alchohol" class="form-label">alchohol</label>
-                            <input type="text" class="form-control" id="alchohol" name="alchohol">
-                            <div class="form-text"></div>
+                            <label for="alchohol" class="form-label">酒量</label>
+                            <div class="form-check">
+                                <input class="form-check-input" type="radio" name="alchohol" id="alchohol" value="滴酒不沾" checked>
+                                <label class="form-check-label" for="flexRadioDefault1">
+                                    滴酒不沾
+                                </label>
+                            </div>
+                            <div class="form-check">
+                                <input class="form-check-input" type="radio" name="alchohol" id="alchohol" value="小酌">
+                                <label class="form-check-label" for="flexRadioDefault2">
+                                    小酌
+                                </label>
+                            </div>
+                            <div class="form-check">
+                                <input class="form-check-input" type="radio" name="alchohol" id="alchohol" value="酒豪">
+                                <label class="form-check-label" for="flexRadioDefault2">
+                                    酒豪
+                                </label>
+                            </div>
                         </div>
                         <div class="mb-3">
-                            <label for="education_level" class="form-label"> education_level</label>
-                            <input type="text" class="form-control" id="education_level" name="education_level">
-                            <div class="form-text"></div>
+                            <label for="education_level" class="form-label">教育程度</label>
+                            <select class="form-select" multiple aria-label="multiple select example" name="education_level">
+                                <option value="國小" selected>國小</option>
+                                <option value="國中">國中</option>
+                                <option value="高中">高中</option>
+                                <option value="學士">學士</option>
+                                <option value="碩士">碩士</option>
+                                <option value="博士">博士</option>
+                            </select>
                         </div>
                         <div class="mb-3">
-                            <label for="job" class="form-label">job</label>
+                            <label for="job" class="form-label">職業</label>
                             <input type="text" class="form-control" id="job" name="job">
                             <div class="form-text"></div>
                         </div>
                         <div class="mb-3">
-                            <label for="profile" class="form-label">profile</label>
+                            <label for="profile" class="form-label">自介</label>
                             <textarea type="text" class="form-control" id="profile" name="profile"></textarea>
                             <div class="form-text"></div>
                         </div>
                         <div class="mb-3">
-                            <label for="mobile" class="form-label">mobile</label>
+                            <label for="mobile" class="form-label">手機</label>
                             <input type="text" class="form-control" id="mobile" name="mobile">
                             <div class="form-text"></div>
                         </div>
 
                         <div class="alert alert-danger" role="alert" id="infoBar" style="display:none"></div>
-
                         <button type="submit" class="btn btn-primary">新增</button>
                     </form>
                 </div>
@@ -183,6 +257,9 @@ require './parts/john_parts/back/part/connect-db.php';
                         infoBar.classList.add('alert-success')
                         infoBar.innerHTML = '新增成功'
                         infoBar.style.display = 'block';
+                        setTimeout(() => {
+                            location.href = "./account.php";
+                        }, 2000);
 
                     } else {
                         infoBar.classList.remove('alert-success')
--- parts/john_parts/back/part/navbar.php
+++ parts/john_parts/back/part/navbar.php
@@ -21,27 +21,19 @@
     #intro {
         background-image: url("./parts/john_parts/back/imgs/ship.jpg");
         background-position: center;
-        /* object-fit: contain; */
         /* width: 100px; */
         height: 85vh;
     }
 </style>
 <div class="container">
-    <nav class="navbar navbar-expand-lg bg-body-tertiary mb-5">
+    <div class="navbar navbar-expand-lg bg-body-tertiary mb-5">
         <div class="container-fluid">
-            <a class="navbar-brand" href="index_.php">Navbar</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link <?= $pageName == 'list' ? 'active' : ' ' ?> " href="account.php">列表</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link <?= $pageName == 'add' ? 'active' : ' ' ?>" href="account_add.php">新增</a>
-                    </li>
-                </ul>
+
+            <div class="collapse navbar-collapse  d-flex justify-content-end" id="navbarSupportedContent">
+
                 <ul class="navbar-nav mb-2 mb-lg-0">
                     <?php if (isset($_SESSION['admin'])) : ?>
                         <li class="nav-item">
@@ -60,7 +52,7 @@
                     <?php endif; ?>
 
                 </ul>
+                <!-- </div> -->
             </div>
         </div>
-    </nav>
-</div>
+    </div>
