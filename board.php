2fa9f8a85ac1681636cfbcbc734315d4a7dc6a20
Thomas <niutingyuan@gmail.com>
2023-05-18 18:02:12 -0800

    Again

--- board.php
+++ board.php
@@ -1,7 +1,12 @@
 <?php include './parts/head.php' ?>
-<title>Test for Layout</title> <!-- 網頁標題可自行修改-->
+<title>Who Post?</title> <!-- 網頁標題可自行修改-->
 <style>
     /* CSS可以自行修改 */
+    @import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap');
+
+    body {
+        font-family: 'Roboto', sans-serif;
+    }
 </style>
 </head>
 
--- parts/niu_parts/index.php
+++ parts/niu_parts/index.php
@@ -9,54 +9,36 @@ if (isset($_POST['deleteData'])) {
 }
 
 ?>
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Who post?</title>
-  <style>
-    @import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap');
-
-    body {
-      font-family: 'Roboto', sans-serif;
-    }
-  </style>
-</head>
 
 <body>
-  <h1>Friend Trip 交友網站留言板後台</h1>
-  <h1>新增一筆貼文</h1>
-  <form action="insert.php" method="post">
-    <input type="text" placeholder="誰發的文" name="name">
-    <input type="email" placeholder="發文者的email" name="email">
-    <input type="text" placeholder="發文在此" name="post">
-    <input type="submit" value="Insert" name="insertData">
-  </form>
+  <div class="container">
+    <h1>Friend Trip 交友網站留言板後台</h1>
+    <h1>新增一筆貼文</h1>
+    <form action="insert.php" method="post">
+      <input type="text" placeholder="誰發的文" name="name">
+      <input type="email" placeholder="發文者的email" name="email">
+      <input type="text" placeholder="發文在此" name="post">
+      <input type="submit" value="Insert" name="insertData">
+    </form>
 
-  <h1>刪除一筆貼文</h1>
-  <form method="post">
-    <input type="text" placeholder="Id" name="id">
-    <input type="submit" value="Delete" name="deleteData">
-  </form>
+    <h1>刪除一筆貼文</h1>
+    <form method="post">
+      <input type="text" placeholder="Id" name="id">
+      <input type="submit" value="Delete" name="deleteData">
+    </form>
 
-  <h1>修改一筆貼文</h1>
-  <form action="editData.php" method="post">
-    <input type="text" placeholder="Id" name="id">
-    <input type="text" placeholder="修改不當貼文內容" name="post">
-    <input type="submit" value="Edit" name="editData">
-  </form>
+    <h1>修改一筆貼文</h1>
+    <form action="editData.php" method="post">
+      <input type="text" placeholder="Id" name="id">
+      <input type="text" placeholder="修改不當貼文內容" name="post">
+      <input type="submit" value="Edit" name="editData">
+    </form>
 
-  <h1>資料庫內容顯示</h1>
-  <?php
-  $data = $db->getData();
-  foreach ($data as $i) {
-    echo $i['id'] . ". " . $i['name'] . " - " . $i['email'] . " - " . $i['post'] . "<br>";
-  }
-  ?>
-
-</body>
-
-</html>
+    <h1>資料庫內容顯示</h1>
+    <?php
+    $data = $db->getData();
+    foreach ($data as $i) {
+      echo $i['id'] . ". " . $i['name'] . " - " . $i['email'] . " - " . $i['post'] . "<br>";
+    }
+    ?>
+  </div>
